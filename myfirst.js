65a86a3814181897c9436fb7d3173d26095972f0
ybeliaev <ybeliaev@gmail.com>
2019-02-16 20:50:45 +0200

    add myfirst

--- myfirst.js
+++ myfirst.js
@@ -4,3 +4,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
 }).listen(8080);
+
+//Start your command line interface, write node myfirst.js and hit enter:
+//
+//D:\myWorks\NodeJS_lessons>node myfirst.js
